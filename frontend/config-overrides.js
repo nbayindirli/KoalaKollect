8f75c9dd03833e8cdceae5c6db616b4287abb26f
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2024-12-05 14:24:45 -0500

    switch to Next.js

--- frontend/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override } = require('customize-cra');
-
-   module.exports = override(
-     (config) => {
-       config.ignoreWarnings = [/Failed to parse source map/];
-       return config;
-     }
-   );
